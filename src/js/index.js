abf008fd53fca8cd645dea24a2e5e26705ae361a
Hefti1879 <s.hefti@etimark.ch>
2020-12-18 12:28:18 +0100

    Added photos, iframe for video and js code for img

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,21 @@
 function showSidenav() {
-    var x = document.getElementById("topNav");
-    if (x.className === "hidden sm:block") {
-      x.className = "sideNav";
+    var el = document.getElementById("topNav");
+    if (el.className === "hidden sm:block") {
+      el.className = "sideNav";
     } else {
-      x.className = "hidden sm:block";
+      el.className = "hidden sm:block";
     }
+}
+
+function switchImage(number){
+
+  var id = "photo" + number;
+  var el = document.getElementById(id);
+  var source = el.src.replace(/^.*[\\\/]/, '');
+  if(source == id + ".jpg"){
+    el.src = "img/" + id + "_edited.jpg";
+  }else {
+    el.src = "img/" + id + ".jpg";
+  }
+
 }
